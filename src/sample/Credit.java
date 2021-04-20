e1da8a62105bcea649e4986cd3b1fec4d10a6477
Ibrahim <ibdem20@student.sdu.dk>
2021-04-20 13:01:43 +0200

    Tilføjet scanner

--- src/sample/Credit.java
+++ src/sample/Credit.java
@@ -1,4 +1,24 @@
 package sample;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Credit {
+    public static void main(String[] args) throws FileNotFoundException {
+
+        //htpp://textfiles.com/100/capmidn.txt
+
+        File file = new File("C:/Brugere/ibo20/Skrivebord/captmidn.txt");
+        Scanner scan  = new Scanner(file);
+
+        System.out.print(scan.nextLine());
+
+        while(scan.hasNextLine()){
+            System.out.println(scan.nextLine());
+        }
+
+
+
+    }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -7,7 +7,6 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
@@ -21,4 +20,5 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
 }
