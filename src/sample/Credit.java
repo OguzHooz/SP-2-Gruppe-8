a7b10bc0458f5a22b8612d27caaeb849f7ef0147
Ibrahim <ibdem20@student.sdu.dk>
2021-04-22 10:15:02 +0200

    Writter + scanner (ik færdig)
    Kan kun få enten Writter og eller scanner til at virke

--- src/sample/Credit.java
+++ src/sample/Credit.java
@@ -2,21 +2,28 @@ package sample;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Credit {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws IOException {
 
         //htpp://textfiles.com/100/capmidn.txt
 
         File file = new File("C:/Users/ibo20/Desktop/hey.txt");
         Scanner scan  = new Scanner(file);
 
-        System.out.print(scan.nextLine());
+        //System.out.print(scan.nextLine());
 
+        String fileContent = "";
         while(scan.hasNextLine()){
-            System.out.println(scan.nextLine());
+            //System.out.println(scan.nextLine());
+            fileContent = fileContent.concat(scan.nextLine() + "\n");
         }
+        FileWriter writer = new FileWriter("C:/Users/ibo20/Desktop/newJavaFileNew.txt");
+        writer.write(fileContent);
+        writer.close();
 
 
 
