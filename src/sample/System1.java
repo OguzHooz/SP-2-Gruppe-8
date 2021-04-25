ac8b0c8febf856208533747307d06d59fda6f808
Christian <donut_qewbs@hotmail.com>
2021-04-25 16:54:01 +0200

    add user
    add user

--- src/sample/System1.java
+++ src/sample/System1.java
@@ -5,4 +5,5 @@ import java.util.List;
 
 public class System1 {
     static List<Person> listOfPersons = new LinkedList<Person>(); //Its a linkedList so its sorted by the last thing that got added
+    static List<User> listOfUsers = new LinkedList<User>();
 }
