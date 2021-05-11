cf1288292580860d143293860261a302e2fc8f92
Lucas <Luhavenkvist@yahoo.dk>
2021-05-11 14:45:22 +0200

    Opsætning af GUI
    fixet gui, samt tilføjet enkelte funktioner indtil videre, resten kommer.

--- src/main/java/org/example/LogInScreen.java
+++ src/main/java/org/example/LogInScreen.java
@@ -1,4 +1,16 @@
 package org.example;
 
 public class LogInScreen {
+
+
+
+    public void startClicked() throws Exception {
+        App.setRoot("StartScreen");
+    }
+    public void loginClicked() throws Exception {
+        App.setRoot("StartScreen");
+    }
+
 }
+
+
